ecada3f51ba431afd3de8920ea9e3ffed5f8f2ae
RaDioDa <teamfeag418@gmail.com>
2021-11-18 21:34:32 +0300

    Добавьте файлы проекта.

---
+++ Alg_lab_4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Alg_lab_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue(100);
+
+            int counter = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                queue.insert("Element " + (i + 1));
+                Console.WriteLine("Element {0}", i + 1);
+            }
+            Console.WriteLine("=================================");
+            Random random = new Random();
+            int min = random.Next(0, 80);
+            int max = random.Next(20, 100);
+            int control = random.Next(0, 100);
+            if (min > max)
+            {
+                int tmp = max;
+                max = min;
+                min = tmp;
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (control >= min && control <= max)
+                {
+                    string tmp = queue.remove();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(tmp + " PASS");
+                    min = random.Next(0, 100);
+                    max = random.Next(0, 100);
+                    control = random.Next(0, 100);
+                    counter++;
+                }
+                else
+                {
+                    string tmp = queue.remove();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(tmp + " NULL");
+                    min = random.Next(0, 100);
+                    max = random.Next(0, 100);
+                    control = random.Next(0, 100);
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("======================\n" +
+                              "= Served: {0} objects =" +
+                            "\n======================", counter);
+        }
+    }
+}
---
+++ Alg_lab_4/Queue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alg_lab_4
+{
+    class Queue
+    {
+        private string[] queue;
+        private int maxSize;
+        private int nElem;
+        private int front;
+        private int rear;
+
+        public Queue(int maxSize)
+        {
+            this.maxSize = maxSize;
+            queue = new string[maxSize];
+            rear = -1;
+            front = 0;
+            nElem = 0;
+        }
+
+        public void insert(string elem)
+        {
+            if (rear == maxSize - 1)
+                rear = -1;
+
+            queue[++rear] = elem;
+            nElem++;
+        }
+
+        public string remove()
+        {
+            string temp = queue[front++];
+
+            if (front == maxSize)
+                front = 0;
+
+            nElem--;
+            return temp;
+        }
+
+        public string getFront() { return queue[front]; }
+        public string getRear() { return queue[rear]; }
+        public bool isFull() {  return nElem == maxSize - 1; }
+        public bool isEmpty() { return nElem == 0; }
+        public int getSize() {  return nElem; }
+    }
+}
